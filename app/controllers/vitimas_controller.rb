0d9eb283007df80c774e0cf36a4fe1ed88f8ad3e
cyd losekann <cydnel@gmail.com>
2024-05-10 03:00:29 -0300

    controller + view de vitimas

--- app/controllers/vitimas_controller.rb
+++ app/controllers/vitimas_controller.rb
@@ -1,19 +1,44 @@
+# app/controllers/vitimas_controller.rb
 class VitimasController < ApplicationController
   def index
+    @vitimas = Vitima.all
   end
 
   def new
+    @vitima = Vitima.new
   end
 
   def create
+    @vitima = Vitima.new(vitima_params)
+    if @vitima.save
+      redirect_to vitimas_path, notice: 'Vítima cadastrada com sucesso.'
+    else
+      render :new
+    end
   end
 
   def edit
+    @vitima = Vitima.find(params[:id])
   end
 
   def update
+    @vitima = Vitima.find(params[:id])
+    if @vitima.update(vitima_params)
+      redirect_to vitimas_path, notice: 'Vítima atualizada com sucesso.'
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @vitima = Vitima.find(params[:id])
+    @vitima.destroy
+    redirect_to vitimas_path, notice: 'Vítima removida com sucesso.'
+  end
+
+  private
+
+  def vitima_params
+    params.require(:vitima).permit(:nome, :telefone, :endereco, :necessidade)
   end
 end
