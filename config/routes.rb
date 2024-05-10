225f587b19392ebf1091a97ce1e5a83743d91c0d
cyd losekann <cydnel@gmail.com>
2024-05-10 19:53:12 -0300

    views

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,5 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-  get 'doadores/index'
-  get 'doadores/new'
-  get 'doadores/create'
-  get 'doadores/edit'
-  get 'doadores/update'
-  get 'doadores/destroy'
-  get 'vitimas/index'
-  get 'vitimas/new'
-  get 'vitimas/create'
-  get 'vitimas/edit'
-  get 'vitimas/update'
-  get 'vitimas/destroy'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :vitimas, only: [:new, :create, :index, :destroy]
+  resources :doadores, only: [:new, :create, :index, :destroy]
+  root 'home#index'
 end
