1652b1ee2989f0a2534349079cc8a300e56c51ca
cyd losekann <cydnel@gmail.com>
2024-05-11 04:41:44 -0300

    conexoes - rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :vitimas, only: [:new, :create, :index, :edit, :update, :destroy]
   resources :doadores, only: [:new, :create, :index, :edit, :update, :destroy]
+  resources :conexoes, only: [:new, :create, :index, :edit, :update, :destroy]
   root 'home#index'
 end
