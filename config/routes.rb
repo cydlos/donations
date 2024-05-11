9fbd9b9e1ecd2040c996b29263413d4c509c3c0c
cyd losekann <cydnel@gmail.com>
2024-05-11 04:01:21 -0300

    routes modified

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :vitimas, only: [:new, :create, :index, :destroy]
-  resources :doadores, only: [:new, :create, :index, :destroy]
+  resources :vitimas, only: [:new, :create, :index, :edit, :update, :destroy]
+  resources :doadores, only: [:new, :create, :index, :edit, :update, :destroy]
   root 'home#index'
 end
